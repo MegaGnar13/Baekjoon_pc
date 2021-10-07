d6db72fddbf7c59ab6e4a37c527bbab6610f54be
Your Name Here <learningssik@gmail.com>
2021-10-07 23:58:08 +0900

    try-except

--- 02.py
+++ 02.py
@@ -1,13 +1,6 @@
-import sys
-A = int(sys.stdin.readline())
-graph_list = []
-for i in range(A):
-    B=list(map(int,sys.stdin.readline().split()))
-
-    B.reverse()
-    graph_list.append(B)
-
-graph_list.sort()
-for i in range(len(graph_list)):
-    a=graph_list[i]
-    print(a[1],a[0])
+while True:
+    try:
+        a=input()
+        print(a)
+    except:
+        break
