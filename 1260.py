91363add7de2068e071b3644916da59b2c4b03e3
Your Name Here <learningssik@gmail.com>
2021-10-16 19:51:46 +0900

    BFS,DFS

---
+++ 1260.py
@@ -0,0 +1,38 @@
+node,edge,start_point = map(int,input().split())
+edge_list=[list(map(int,input().split())) for j in range(edge)]
+next_node=dict()
+for i in range(1,node+1):
+    connect=[]
+    for j in edge_list:
+        if i in j:
+            j.remove(i)
+            connect.extend(j)
+            j.append(i)
+            connect.sort()
+    next_node[i]=connect
+
+def DFS(next_node,startpoint):
+    stack=[]
+    answer=[]
+    stack.append(start_point)
+    while stack:
+        n=stack.pop()
+        if n not in answer:
+            answer.append(n)
+            stack.extend(reversed(next_node[n]))
+    print(*answer)
+    return
+DFS(next_node,start_point)
+
+def BFS(next_node,startpoint):
+    queue=[]
+    answer=[]
+    queue.append(start_point)
+    while queue:
+        n=queue.pop(0)
+        if n not in answer:
+            answer.append(n)
+            queue.extend(next_node[n])
+    print(*answer)
+    return
+BFS(next_node,start_point)
