21f06d40068dd8b3ef2c6b58aba19574b8af1152
Your Name Here <learningssik@gmail.com>
2021-11-04 21:49:55 +0900

    ..

--- 01.py
+++ 01.py
@@ -1,24 +1,3 @@
-import sys
-A,B,C = map(int,sys.stdin.readline().split())
-mark=[list(map(int,sys.stdin.readline().split())) for i in range(A)]
-set_mark=set()
-for i in mark:
-    set_mark.update(i)
-answer=[]
-for height in set_mark:
-    num=0
-    block=C
-    for x in range(A):
-        for y in range(B):
-            if mark[x][y]>height:
-                num+=(mark[x][y]-height)*2
-                block-=1
-            elif mark[x][y]<height:
-                num+=(height-mark[x][y])
-                block-=1
-            else:
-                continue
-    if block>=0:
-        answer.append([num,height])
-answer.sort(key=lambda x:(x[0],x[1]))
-print(*answer[0])
+val = "123".rjust(8, " ")
+print(val)
+
--- 02.py
+++ 02.py
@@ -1,14 +1,26 @@
-A=int(input())
-cnt=0
-count=0
-while True:
-    if cnt>=A:
-        break
-    count+=1
-    cnt+=count
+def change_currency(sentence, rate):
+    menu_1 = sentence.splitlines()
+    menu_dic = []
+    for i in menu_1:
+        i = i.split()
+        menu_dic.append([i[0],i[2]])
+    print(menu_dic)
 
-a=(A-(cnt-count))
-if count%2==0:
-    print(a,'/',count+1-a,sep='')
-else:
-    print(count + 1 - a, '/',a , sep='')
+    answer=[]
+    for i in menu_dic:
+        i[1]=float(i[1])
+        i[1]*=rate
+        i[1]=str(i[1])
+        aa=' '.join([i[0],':',i[1],'KWN'])
+        answer.append(aa)
+    rrr='\n'.join(answer)
+    return rrr
+
+menu = """Apple : 1.5 USD
+Banana : 1 USD
+Cake : 7 USD
+"""
+
+usd = 1200.0 # 1usd = 1200kwn
+result = change_currency(menu, usd)
+print(result)
