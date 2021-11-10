75328dfccd46bf2afcb2bd6d144b812564fd756e
Your Name Here <learningssik@gmail.com>
2021-11-10 20:38:18 +0900

    class_2

--- 01.py
+++ 01.py
@@ -1,39 +1,26 @@
-class MyCounter():
-    """Simple Counter class"""
-    n = 0
+class BankAccount():
+    def __init__(self, balance=0):
+        self.balance = balance
 
-    def __init__(self, value=0):
-        """Initialize counter with given value or default value (0)"""
-        self.counter = value
-        MyCounter.n += 1
+    def withdraw(self, amount):
+        self.balance-=amount
+        return self.balance
 
-    def get(self):
-        """Return the value of counter"""
-        return self.counter
 
-    def set(self, value):
-        self.counter=value
-        """Set the counter with the given value"""
-        pass
+    def deposit(self, amount):
+        self.balance+=amount
+        return self.balance
 
-    def inc(self):
-        """Increase counter by 1"""
-        self.counter += 1
+    def getBalance(self):
+        return self.balance
 
-    def dec(self):
-        self.counter-=1
-        """Decrease counter by 1"""
-        pass
 
 if __name__ == '__main__':
-    c1 = MyCounter()
-    c2 = MyCounter(42)
-    print(MyCounter.n)
-    print(c1.get(), c2.get())
-    c1.inc()
-    c1.inc()
-    c2.dec()
-    print(c1.get(), c2.get())
-    c1.set(23)
-    c2.set(1024)
-    print(c1.get(), c2.get())
+    b1 = BankAccount(500)
+    b2 = BankAccount(2000)
+    b1.withdraw(400)
+    print(b1.getBalance())
+    b2.deposit(1000)
+    print(b2.getBalance())
+    b2.withdraw(5000)
+    print(b2.getBalance())
