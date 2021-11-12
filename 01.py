1ea9a5491b92ea30d22a451ff01562ce15cad061
Your Name Here <learningssik@gmail.com>
2021-11-12 18:35:59 +0900

    programming_hw_2-1

--- 01.py
+++ 01.py
@@ -1,26 +1,43 @@
-class BankAccount():
-    def __init__(self, balance=0):
-        self.balance = balance
+def circular(n):
+    n=list(str(n))
+    result=[]
+    cnt=0
+    while cnt<len(n):
+        a=''.join(n)
+        result.append(int(a))
+        b=n[0]
+        del n[0]
+        n.append(b)
+        cnt+=1
+    return result
 
-    def withdraw(self, amount):
-        self.balance-=amount
-        return self.balance
+def prime(n):
+    cnt=0
+    for i in range(2,n):
+        if n%i==0:
+            cnt+=1
+    if cnt==0:
+        return True
+    else:
+        return False
+
+def circular_prime(n):
+    cir=circular(n)
+    cnt=0
+    for i in cir:
+        if prime(i)==True:
+            cnt+=1
+    if cnt==len(cir):
+        return True
+    else:
+        return False
 
 
-    def deposit(self, amount):
-        self.balance+=amount
-        return self.balance
-
-    def getBalance(self):
-        return self.balance
-
-
-if __name__ == '__main__':
-    b1 = BankAccount(500)
-    b2 = BankAccount(2000)
-    b1.withdraw(400)
-    print(b1.getBalance())
-    b2.deposit(1000)
-    print(b2.getBalance())
-    b2.withdraw(5000)
-    print(b2.getBalance())
+print(circular(123))
+print(circular(1123))
+print(prime(2))
+print(prime(3))
+print(prime(4))
+print(circular_prime(197))
+print(circular_prime(13))
+print(circular_prime(19))
