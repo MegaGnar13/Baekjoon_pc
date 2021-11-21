4127f6e9b55cfca5e4204e59e4bd8fdb915adeca
Your Name Here <learningssik@gmail.com>
2021-11-21 17:03:52 +0900

    datatime

--- 03.py
+++ 03.py
@@ -1,24 +1,19 @@
-def load_dict(filename):
-    answer={}
-    with open(filename, "rt") as f:
-        a=f.read()
-    b=a.splitlines()
-    for i in range(len(b)):
-        c=b[i].split(':')
-        answer[c[0]]=c[1]
-    return answer
-def save_dict(filename, my_dict):
-    with open(filename,'wt') as f:
-        for i in my_dict:
-            print(i,':',my_dict[i])
-    pass
+from datetime import datetime
 
-my_dict = load_dict('mydictionary.txt')
-my_dict['strawberry'] = "A strawberry is a small red fruit which is soft and juicy and has tiny yellow seeds on its skin."
-save_dict('mydictionary.txt', my_dict)
-# http://10.150.4.67/files/본인학번_dict.txt 을 접속하여 잘 저장되었는지 확인 (예:  http://10.150.4.67/files/202111000_dict.txt )
+start="05/04 MON"
+what="06/12"
+print(start[-3:])
+start=start[:-4]
+start=start.split("/")
+what=what.split("/")
+start=''.join(start)
+what=''.join(what)
+# print(start,what)
 
-my_dict = load_dict('mydictionary.txt')
-my_dict['test'] = "test."
-save_dict('mydictionary.txt', my_dict)
-# http://10.150.4.67/files/본인학번_dict.txt 을 접속하여 잘 저장되었는지 확인
+now=datetime.strptime(what,"%m%d")
+
+date_to_compare=datetime.strptime(start,"%m%d")
+
+date_diff=now-date_to_compare
+print(date_diff.days)
+
