93513b0e166fe050018393b8b13438a233b8852f
Your Name Here <learningssik@gmail.com>
2022-03-24 12:39:51 +0900

    골드바흐 추측

--- Baekjoon/9020.py
+++ Baekjoon/9020.py
@@ -1,27 +1,12 @@
-def prime_num(num):
-    pan = [True]*num
-    m = int(num**(0.5))
-    for i in range(2, m+1):
-        if pan[i] == True:
-            for j in range(i+i, num, i):
-                pan[j] = False
-
-    return [i for i in range(2,num) if pan[i] == True]
-
-i = int(input())
-pr = prime_num(10000)
-for _ in range(i):
-    ans = []
-    num = int(input())
-    A = num // 2
-    B = A
-    while True:
-
-        if A in pr and B in pr:
-            print(A, B)
-            break
-        else:
-            A-=1
-            B+=1
-
-
+a = 9
+maxnum = 0
+time = 0
+ans = []
+for i in range(a):
+    b = int(input())
+    time+=1
+    if maxnum<b:
+        maxnum = b
+        ans = [b,time]
+print(ans[0])
+print(ans[1])
