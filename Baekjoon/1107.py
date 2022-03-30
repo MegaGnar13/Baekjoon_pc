f3013c242f4aa801ed9c58e3a6d3271e839df05c
Your Name Here <learningssik@gmail.com>
2022-03-30 14:58:33 +0900

    완전 탐색 - 풀이 추가

--- Baekjoon/1107.py
+++ Baekjoon/1107.py
@@ -3,9 +3,6 @@
  내 풀이
  end를 기준으로 +,- 1씩 시켜서 조건을 만족하는 값까지 찾기
 
-
- 다른 사람 풀이
- 나올 수 있는 모든 채널을 처음부터 끝까지 탐색 --> 탐색할 때마다 최소값 업데이트
 '''
 end = int(input())
 bn = int(input())
@@ -65,3 +62,22 @@ else:
         print(min(abs(end-100), len(str(ans))+abs(ans-end) ))
 
 
+'''
+ 다른 사람 풀이
+ 나올 수 있는 모든 채널을 처음부터 끝까지 탐색 --> 탐색할 때마다 최소값 업데이트
+'''
+N = int(input())
+M = int(input())
+btns = set()  # 고장난 버튼 집합
+if 0 != M:  # 고장난 버튼 있으면 입력
+    btns = set(input().split())
+ans = abs(N - 100)  # 100부터 N으로 +나 -버튼 누른 횟수
+for num in range(1_000_001):  # 0 ~ 1,000,000 채널 번호
+    str_num = str(num)  # 채널 번호를 문자열로
+    for n in str_num:  # 채널 번호의 한 자리씩
+        if n in btns:  # 해당 자리가 고장난 버튼 숫자이면 종료
+            break
+    else:  # 채널 번호의 모든 자리가 고장나지 않은 경우
+        print(num)
+        ans = min(ans, len(str_num) + abs(num - N))
+print(ans)
