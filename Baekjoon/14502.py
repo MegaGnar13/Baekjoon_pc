e041033fc790d54d2011e970d5fd65c3b1006ee2
Your Name Here <learningssik@gmail.com>
2022-05-16 20:37:32 +0900

    구현 그래프 브루트포스

---
+++ Baekjoon/14502.py
@@ -0,0 +1,19 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+
+lab = []
+
+for i in range(n):
+    li = list(map(int, sys.stdin.readline().split()))
+    lab.append(li)
+ans = []
+
+for i in range(n):
+    for j in range(m):
+        if lab[i][j] == 0:
+
+
+def zeropadding
+
+def dfs():
