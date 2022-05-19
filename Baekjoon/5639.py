fae01ccd0a7352dfeaa2b1c3f15014ee9386271c
Your Name Here <learningssik@gmail.com>
2022-05-19 18:38:54 +0900

    이진 트리의 순회

---
+++ Baekjoon/5639.py
@@ -0,0 +1,30 @@
+import sys
+sys.setrecursionlimit(10**6)
+num_list = []
+while True:
+    try:
+        num = int(input())
+        num_list.append(num)
+    except:
+        break
+
+# print(num_list)
+
+def postorder(first, end):
+
+    if end < first:
+        return
+
+    mid = end + 1
+
+    for i in range(first+1, end+1):
+        if num_list[first] < num_list[i]:
+            mid = i
+            break
+
+    postorder(first+1,mid-1)
+    postorder(mid,end)
+    print(num_list[first])
+
+
+postorder(0,len(num_list)-1)
