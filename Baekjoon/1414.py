09c6b6d496eda258148f82b476548f2aa78bd1e5
Your Name Here <learningssik@gmail.com>
2022-06-21 16:44:13 +0900

    MST - 수정: 모든 parent 값이 같지 않아도 연결될 수 있다.

--- Baekjoon/1414.py
+++ Baekjoon/1414.py
@@ -7,8 +7,8 @@ parent = [i for i in range(computer+1)]
 ans = 0
 check = []
 
-def find(k):
-    if not parent[k] == k:
+def find(k:int) -> int:
+    if parent[k] != k:
         parent[k] = find(parent[k])
 
     return parent[k]
@@ -39,26 +39,35 @@ for i in range(computer):
             else:
                 if cc[j].islower():
                     check.append([ord(cc[j])-96, i+1, j+1])
+                    ans += ord(cc[j])-96
                 else:
                     check.append([ord(cc[j])-38, i+1, j+1])
-
+                    ans += ord(cc[j])-38
 check.sort()
+tar = 0
+temp = []
 
 for i in range(len(check)):
     # print(check[i])
 
     if find(check[i][1]) != find(check[i][2]):
+        tar += check[i][0]
         union(check[i][1], check[i][2])
-    else:
-        ans += check[i][0]
-
-err = 0
-print(parent)
-for i in range(1, computer):
-    if parent[i] != parent[i+1]:
-        err += 1
+        temp.append(i)
+    if len(temp) == computer - 1:
         break
-if err == 0:
-    print(ans)
-else:
+
+if len(temp) < computer-1:
     print(-1)
+else:
+    print(ans-tar)
+
+# err = 0
+# for i in range(1, computer):
+#     if parent[i] != parent[i+1]:
+#         err += 1
+#         break
+# if err == 0:
+#     print(ans - tar)
+# else:
+#     print(-1)
