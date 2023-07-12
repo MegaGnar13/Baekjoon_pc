e56dbc790c2921dcd7196ca1a973367b129c09fe
learningssik <learningssik@gmail.com>
2023-07-12 21:11:22 +0900

    동기화

--- Baekjoon/03.py
+++ Baekjoon/03.py
@@ -1,131 +1,33 @@
-from collections import deque
-h, w = map(int, input().split())
-m = []
+n, m = map(int,input().split())
+chess = []
+for i in range(n):
+    chess.append(list(input()))
 
-for _ in range(h):
-    m.append(list(input()))
+# print(chess)
 
-r = []
-b = []
-hole = []
-for i in range(h):
-    for j in range(w):
-        if m[i][j] == 'R':
-            r = [i, j]
-        if m[i][j] == 'B':
-            b = [i, j]
-        if m[i][j] == 'O':
-            hole = [i, j]
+ans = float('inf')
 
-dx = [0, 0, -1, 1]
-dy = [1, -1, 0, 0]
-cnt = 0
-err = 0
-queue = deque([[r[0], r[1], b[0], b[1], cnt]])
+for i in range(n-7):
+    for j in range(m-7):
+        cnt = 0
+        ccnt = 0
+        for k in range(i,i+8):
+            for l in range(j,j+8):
+                # W start
+                if (k+l)%2 == 1:
+                    if chess[k][l] == 'W':
+                        cnt += 1
+                    if chess[k][l] == 'B':
+                        ccnt += 1
 
-tmp = [[[[0 for i in range(w)] for j in range(h)] for k in range(w)] for l in range(h)]
-tmp[r[0]][r[1]][b[0]][b[1]] = 1
+                else:
+                    if chess[k][l] == 'B':
+                        cnt += 1
+                    if chess[k][l] == 'W':
+                        ccnt += 1
+        if ans > min(cnt, ccnt):
+            ans = min(cnt, ccnt)
 
-final = 0
-
-while queue:
-
-    next = queue.popleft()
+print(ans)
 
 
-    if next[4] > 10:
-        err += 1
-        break
-
-    # print(next)
-
-    if m[next[0]][next[1]] == 'O':
-        if m[next[2]][next[3]] == 'O':
-            continue
-        else:
-            final = next[4]
-            break
-    if m[next[2]][next[3]] == 'O':
-        continue
-
-
-    m[next[0]][next[1]] = 'R'
-    m[next[2]][next[3]] = 'B'
-
-    for i in range(4):
-
-        start_red_x = next[0]
-        start_red_y = next[1]
-
-        red_x = next[0]
-        red_y = next[1]
-
-        end_red_x = 0
-        end_red_y = 0
-
-        while True:
-            nx = red_x + dx[i]
-            ny = red_y + dy[i]
-
-            if m[nx][ny] == '.' or m[nx][ny] == 'B':
-                red_x = nx
-                red_y = ny
-            elif m[nx][ny] == '#':
-                end_red_x = red_x
-                end_red_y = red_y
-                break
-            else:
-                end_red_x = nx
-                end_red_y = ny
-                break
-
-        start_blue_x = next[2]
-        start_blue_y = next[3]
-
-        blue_x = next[2]
-        blue_y = next[3]
-
-        end_blue_x = 0
-        end_blue_y = 0
-
-        while True:
-            nx = blue_x + dx[i]
-            ny = blue_y + dy[i]
-
-            if m[nx][ny] == '.' or m[nx][ny] == 'R':
-                blue_x = nx
-                blue_y = ny
-            elif m[nx][ny] == '#':
-                end_blue_x = blue_x
-                end_blue_y = blue_y
-                break
-            else:
-                end_blue_x = nx
-                end_blue_y = ny
-                break
-
-
-
-        if end_blue_y == end_red_y and end_blue_x == end_red_x and m[end_red_x][end_red_y] != 'O':
-            if abs(end_blue_x - start_blue_x) + abs(end_blue_y - start_blue_y) > abs(end_red_x - start_red_x) + abs(end_red_y - start_red_y):
-
-                end_blue_x -= dx[i]
-                end_blue_y -= dy[i]
-            else:
-                end_red_x -= dx[i]
-                end_red_y -= dy[i]
-
-        if tmp[end_red_x][end_red_y][end_blue_x][end_blue_y] == 1:
-            continue
-
-        tmp[end_red_x][end_red_y][end_blue_x][end_blue_y] = 1
-        queue.append([end_red_x, end_red_y, end_blue_x, end_blue_y, next[4] + 1])
-
-    m[next[0]][next[1]] = '.'
-    m[next[2]][next[3]] = '.'
-
-    # print(queue)
-if err == -1 or final == 0:
-    print(-1)
-else:
-    print(final)
